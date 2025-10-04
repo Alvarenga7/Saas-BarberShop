e8766427a30a647416115a3c6c4b971c35d9f324
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-10-04 19:02:29 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
-import { Button } from "@/components/ui/button"
+import { Input } from "@/app/components/ui/input"
+import Header from "./components/ui/header"
+import { Button } from "./components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      <Button>Teste</Button>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Henrique!</h2>
+        <p>Sabao, 04 de outubro</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Home
